2e80b8a32f2734168f3f6375322b3fa491b3717e
SprytnyMarek <jtytanowy69@gmail.com>
2022-06-01 15:21:30 +0200

    test and samll fixes

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
-
+from django.test import Client
 # Create your tests here.
+
+class ViewsTestCase(TestCase):
+    client = Client()
+
+    example_movie_id = 414906
+    example_movie_name = "Django"
+
+    def test_getMovieImages(self):
+        response = self.client.get(
+            '/movies/' + str(self.example_movie_id) + "/images")
+        self.assertEqual(response.status_code, 200)
+
+    def test_getMovieCrew(self):
+        response = self.client.get(
+            '/movies/' + str(self.example_movie_id) + "/credits")
+        self.assertEqual(response.status_code, 200)
+
+    def test_getSimilarMovies(self):
+        response = self.client.get(
+            '/movies/' + str(self.example_movie_id) + "/similar")
+        self.assertEqual(response.status_code, 200)
+
+    def test_getTrendingMovies(self):
+        response = self.client.get(
+            '/movies/trending'
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_getMoviesByName(self):
+        response = self.client.get(
+            '/search/movie/page/1/name/' + str(self.example_movie_id))
+        self.assertEqual(response.status_code, 200)
